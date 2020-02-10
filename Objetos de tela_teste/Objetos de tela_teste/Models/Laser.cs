20d6ff8446f685addd6efdc69e295e5c70af02de
Guigahick <guilherme.henriquedev@gmail.com>
2020-02-10 18:12:19 -0300

    add Laser class

---
+++ Objetos de tela_teste/Objetos de tela_teste/Models/Laser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos_de_tela_teste.Models
+{
+    public class Laser
+    {
+        public int ID { get; set; }
+
+        public byte MinPowerCurrent { get; set; }
+
+        public byte MaxPowerCurrent { get; set; }
+
+        public byte Increment { get; set; }
+
+        public byte DesiredTemperature { get; set; }
+
+        public byte[] GetByteArray()
+        {
+            byte[] result = new byte[32];
+            result[0] = 0x00;
+            result[1] = (byte)'L';
+            result[2] = (byte)'S';
+            result[3] = MinPowerCurrent;
+            result[4] = MaxPowerCurrent;
+            result[5] = Increment;
+            result[6] = DesiredTemperature;
+            result[7] = (byte)ID;
+            result[8] = (byte)ID;
+
+            return result;
+        }
+    }
+}
